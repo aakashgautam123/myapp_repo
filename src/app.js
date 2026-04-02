5319197d6857cc0638cd9b08dca4a5146744ce46
Aakash Gautam <aakashgautam107@gmail.com>
2026-04-02 21:59:04 +0545

    route new contetn home

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) =>{
-  res.send("Welcome to the Continuous Integration/Deployment with zero downtime latest");
+  res.send("totally new");
 });
 
 app.get("/health", (req, res) => {
