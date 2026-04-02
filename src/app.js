ae8931ea15ed782073af4f328345a7e4c3c3bae6
Aakash Gautam <aakashgautam107@gmail.com>
2026-04-02 21:40:49 +0545

    api edited

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) =>{
-  res.send("Welcome to the Continuous Integration/Deployment with zero downtime");
+  res.send("Welcome to the Continuous Integration/Deployment with zero downtime latest");
 });
 
 app.get("/health", (req, res) => {
