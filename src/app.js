ec4929aaa5c41d244a2c6cb1b16a230bac0f4505
Aakash Gautam <aakashgautam107@gmail.com>
2026-04-04 14:18:11 +0545

    nginx fix added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) =>{
-  res.send("totally new");
+  res.send("totally new with nginx fix");
 });
 
 app.get("/health", (req, res) => {
